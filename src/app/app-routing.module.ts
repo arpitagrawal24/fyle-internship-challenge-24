bfedd108edb72c165715a2750a947d6a70c93f92
Arpit <arpitagrawal2402@gmail.com>
2024-07-19 10:52:44 +0530

    Remove unused route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
 import { WorkoutListComponent } from './components/workout-list/workout-list.component';
-import { WorkoutChartComponent } from './components/workout-chart/workout-chart.component';
 
 const routes: Routes = [
   { path: 'add-workout', component: WorkoutFormComponent },
   { path: 'workout-list', component: WorkoutListComponent },
-  { path: 'workout-chart', component: WorkoutChartComponent },
   { path: '', redirectTo: '/workout-list', pathMatch: 'full' },
 ];
 
